f7d185a3400b83a31e187999f1572f8373cf6e45
k1mminho <brightdeep@naver.com>
2024-07-01 15:48:03 +0900

    server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = process.env.PORT;
 const express = require("express");
 const https = require("https");
 const fs = require("fs");
-const option = {
+const options = {
   key: fs.readFileSync("./cert/cert.key"),
   cert: fs.readFileSync("./cert/cert.crt"),
 };
@@ -11,8 +11,61 @@ const app = express();
 const cors = require("cors");
 app.use(
   cors({
-    origin : true,
-    credentials : true,
+    origin: true,
+    credentials: true,
   })
-)
-const path = require('path');
+);
+const path = require("path");
+const models = require("./models");
+const {sequelize, Op} = require("sequelize")
+const session = require('express-session')
+const MySQLStore = require('express-mysql-session')(session)
+const passport = require('passport')
+
+const dbConfig = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+};
+
+const sessionConfig = session({
+  secret: "1111",
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 1000 * 60 * 60, secure: true, httpOnly: false, sameSite: 'none' },
+  store: new MySQLStore(dbConfig),
+});
+
+app.use(express.static(path.join(__dirname + "/public")))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(sessionConfig);
+app.use(passport.initialize());
+app.use(passport.session());
+
+const passportConfig = require('./passport')
+passportConfig()
+
+app.listen(11111, () => {
+  console.log(`http://192.168.6.8:11111`);
+});
+
+const server = https.createServer(options, app).listen(port, () => {
+  console.log(`https://192.168.6.8:${port}`);
+});
+
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: true,
+    credentials: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+  res.send({ userInfo: req.user })
+});
+
